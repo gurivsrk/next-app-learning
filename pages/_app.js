7639f3edefc12a1d06ca8221ad8eefd49a0a3ea4
Gursharan Singh <gursharan@vsrkcapital.com>
2022-05-16 18:29:28 +0530

    added global css and layout

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/global.css';
+
+export default function App({pageProps, Component}) {
+    return <Component {...pageProps}/>;
+}
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,33 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return(
-        <>
+        <Layout>
         <Head>
             <title>First Post</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta property="og:title" content="My page title" key="title" />
+            <link rel="icon" href="/favicon.png" />
+             {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta property="og:title" content="The Rock" />
+            <meta property="og:type" content="video.movie" />
+            <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+            <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" /> */}
+             {/* <script src="https://connect.facebook.net/en_US/sdk.js" /> */}
         </Head>
-        <Head>
-            <meta property="og:title" content="My new title" key="title" />
-        </Head>
-        <h1>First Post</h1>
-        <Link href='/'>
-            <a>Back to home</a>
+        <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy='lazyOnload'
+        onLoad={()=>console.log('script lazyOnload properly')}
+        />
+        <h1>First Post
+            {' '}
+        <Link  href='/'>
+            <a className='backToHome'>Back to home</a>
         </Link>
-        </>
+        </h1>
+        
+        </Layout>
     )
 }
